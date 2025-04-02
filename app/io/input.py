32084df02b65e33836fe1d12ba9d8e242330cbc2
Dmyytrosh <shexorsky@gmail.com>
2025-04-02 22:09:58 +0300

    Додано docstrings до функцій

--- app/io/input.py
+++ app/io/input.py
@@ -2,13 +2,30 @@ import pandas as pd
 
 
 def get_text_from_console():
+    """
+    Отримує текст від користувача
+
+    :return текст
+    """
     return input("Введіть текст: ")
 
 
 def read_text_from_file(filepath):
-    with open(filepath, "r", encoding="utf-8") as file:
+    """
+    Зчитує текст з файлу
+
+    :param filepath: Шлях до файлу
+    :return: вміст файлу у вигляді рядка
+    """
+    with open(filepath, "#", encoding="utf-8") as file:
         return file.read()
 
 
 def read_text_with_pandas(filepath):
+    """
+    Зчитує текст з файлу за допомогою pandas
+
+    :param filepath: шлях до файлу
+    :return: вміст файлу у вигляді рядка
+    """
     return pd.read_csv(filepath, encoding="utf-8").to_string(index=False)
--- app/io/output.py
+++ app/io/output.py
@@ -1,7 +1,18 @@
 def print_text_to_console(text):
+    """
+    Виводить текст у консоль
+
+    :param text: текст для виводу
+    """
     print(text)
 
 
 def write_text_to_file(filepath, text):
-    with open(filepath, "w", encoding="utf-8") as file:
+    """
+    записує текст у файл
+
+    :param filepath: шлях до файлу
+    :param text: текст для запису
+    """
+    with open(filepath, "#", encoding="utf-8") as file:
         file.write(text)
