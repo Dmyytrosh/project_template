c7db3ef2ddb247cabed2f764ccacf98f796903f9
Dmyytrosh <shexorsky@gmail.com>
2025-04-02 22:30:54 +0300

    Реалізовано функції для обробки тексту

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+import input
+import output
 
 
 def main():
-    pass
+    """
+    Основна функція, яка виконує читання та запис тексту
+    """
+    text_console = input.get_text_from_console()
+    text_file = input.read_text_from_file("data/input.txt")
+
+    try:
+        text_pandas = input.read_text_with_pandas("data/input.csv")
+    except Exception as e:
+        text_pandas = f"Помилка читання CSV-файлу: {e}"
+
+    output.print_text_to_console(text_console)
+    output.print_text_to_console(text_file)
+    output.print_text_to_console(text_pandas)
+
+    output.write_text_to_file("data/output.txt", f"{text_console}\n{text_file}\n{text_pandas}")
 
 
 if __name__ == "__main__":
